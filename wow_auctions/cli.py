159a178f9bc7faf70ec2e4c7aca0e2fba69aab2e
Jason Hopper <jason.t.hopper@gmail.com>
2016-11-29 18:45:46 -0400

    will pull idtem data

--- wow_auctions/cli.py
+++ wow_auctions/cli.py
@@ -2,7 +2,7 @@
 # @Date:   2016-11-11
 # @Email:  hopperj@ampereinnotech.com
 # @Last modified by:   hopperj
-# @Last modified time: 2016-11-28
+# @Last modified time: 2016-11-29
 # @License: GPL3
 
 
@@ -36,9 +36,10 @@ log_levels = {
 @click.option('--db-name', default='wow_auctions', required=False)
 @click.option('--db-url-name', default='urls', required=False)
 @click.option('--db-data-name', default='auctions', required=False)
+@click.option('--db-item-name', default='items', required=False)
 @click.option('--api-key', required=True)
 @click.pass_context
-def run(ctx, log_level, log, db_addr, db_port, db_name, db_url_name, db_data_name, api_key):
+def run(ctx, log_level, log, db_addr, db_port, db_name, db_url_name, db_data_name, db_item_name, api_key):
 
 
     log_level = log_levels[log_level]
@@ -60,20 +61,34 @@ def run(ctx, log_level, log, db_addr, db_port, db_name, db_url_name, db_data_nam
     ctx.obj.db = connection[db_name]
     ctx.obj.urls_collection = ctx.obj.db[db_url_name]
     ctx.obj.data_collection = ctx.obj.db[db_data_name]
+    ctx.obj.item_collection = ctx.obj.db[db_item_name]
 
     ctx.obj.api_key = api_key
 
+
+# @run.command()
+# @click.argument('item_id', type=int, required=True)
+# @click.pass_context
+def get_item(item_collection, item_id, api_key):
+
+    if item_collection.find({'id':item_id}).count():
+        return
+
+    logging.info('getting item: %d'%item_id)
+    item_data = json.loads(
+        requests.get(
+            'https://us.api.battle.net/wow/item/%d?locale=en_US&apikey=%s'%(item_id, api_key)
+        ).text
+    )
+
+
+    item_collection.insert(item_data)
 
 
 
 @run.command()
 @click.pass_context
 def pull(ctx):
-
-    # print('ctx.fake:',ctx.obj.fake)
-    # return
-
-
 
     logging.debug('Getting new AH data')
     url_data = json.loads(
@@ -82,41 +97,27 @@ def pull(ctx):
         ).text
     )['files'][0]
 
-    # url_data['lastModified'] = datetime.fromtimestamp(url_data['lastModified']/1e3)
 
     if ctx.obj.urls_collection.find({'lastModified':url_data['lastModified']}).count():
         logging.info('No update found')
         return
 
 
+    auction_data = json.loads(
+        requests.get(
+            url_data['url']
+        ).text
+    )
+
     ctx.obj.data_collection.insert(
-        json.loads(
-            requests.get(
-                url_data['url']
-            ).text
-        )
+        auction_data,
     )
     ctx.obj.urls_collection.insert(url_data)
 
     logging.info('Data pulled for timestamp: %d'%url_data['lastModified'])
 
-    url_data = json.loads(
-        requests.get(
-            'https://us.api.battle.net/wow/auction/data/wildhammer?locale=en_US&apikey=ntytntct4askxh25hxwje4gvtdhuahkw'
-        ).text
-    )
 
-    print(ctx.obj.api_key)
-
-    # return
-
-    # if ctx.obj.urls_collection.find({'lastModified':url_data['lastModified']}).count():
-    #     logging.info('No update found')
-    #     return
-    #
-    # logging.debug('Getting new AH data')
-
-
-
-# if __name__ == '__main__':
-#     run()
+    for auction in auction_data['auctions']:
+        # print('doing item: ',auction['item'])
+        # print(auction['item'])
+        get_item(ctx.obj.data_collection, auction['item'], ctx.obj.api_key)
